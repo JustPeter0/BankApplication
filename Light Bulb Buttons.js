8ed8cad4e47631d0e3b028ebd4c914d62d1ece1c
Elizabeth A. Fujita <syp012@c28-0113-26.ad.mtu.edu>
2019-07-12 13:39:09 -0400

    Bank Code

---
+++ Light Bulb Buttons.js
@@ -0,0 +1,16 @@
+function turnlightoff(){
+    document.getElementById('bulb').src = 
+    "https://www.w3schools.com/js/pic_bulboff.gif"
+}
+
+
+function turnlighton(){
+    document.getElementById('bulb').src = 
+    "https://www.w3schools.com/js/pic_bulbon.gif"
+}
+
+// String
+// Number
+// Boolean
+// Null
+// undefined
